43bcf3c62a83b162fe0ee302c32929845313b4ff
tepythai <thaitepy@gmail.com>
2020-05-26 17:51:34 +0900

    fix link

--- src/components/meta.js
+++ src/components/meta.js
@@ -8,8 +8,7 @@ export default ({ title, description, image, url, keywords }) => (
       content={image ? 'summary_large_image' : 'summary'}
     />
     {title && <meta name="twitter:title" property="og:title" content={title} />}
-    {url && <meta property="og:url" content={url} />}
-    {/* {description && <meta property="description" content={description} />} */}
+    {/* {url && <meta property="og:url" content={url} />} */}
     {description && <meta property="og:description" content={description} />}
     {image && <meta property="og:image" content={`https://tepy.dev${image}`} />}
     {keywords && <meta property="keywords" content={keywords} />}
