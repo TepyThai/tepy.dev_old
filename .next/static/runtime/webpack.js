70a270ad7e2bced94b344a7e89d17094e16f8c7d
tepythai <thaitepy@gmail.com>
2020-05-26 17:21:19 +0900

    fix link

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8c5bd9735c1751d391f0";
+/******/ 	var hotCurrentHash = "4736a5bfa8712c0275a1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/about.js
+++ src/pages/about.js
@@ -44,30 +44,26 @@ function About() {
               than happy if you would consider me.
             </div>
             <div>You can contact me via the following options:</div>
-            <ul className="w-64 mx-auto">
-              <li className="my-4 flex justify-center items-center bg-github text-center text-white rounded-full max-w-xs cursor-pointer hover:bg-gray-200 hover:text-github transform hover:-translate-y-1 transition duration-200 ease-in-out">
-                <a
-                  href="https://github.com/tepythai"
-                  target="_blank"
-                  rel="noopener"
-                  className="mr-2"
-                >
-                  GitHub
-                </a>
+            <div className="w-64 mx-auto">
+              <a
+                href="https://github.com/tepythai"
+                target="_blank"
+                rel="noopener"
+                className="my-4 flex justify-center items-center bg-github text-center text-white rounded-full max-w-xs cursor-pointer hover:bg-gray-200 hover:text-github transform hover:-translate-y-1 transition duration-200 ease-in-out"
+              >
+                <span className="mr-2">GitHub</span>
                 <FaGithub />
-              </li>
-              <li className="flex justify-center items-center bg-twitter text-center text-white rounded-full max-w-xs cursor-pointer hover:bg-gray-200 hover:text-twitter transform hover:-translate-y-1 transition duration-200 ease-in-out">
-                <a
-                  href="https://twitter.com/TepyThai"
-                  target="_blank"
-                  rel="noopener"
-                  className="mr-2"
-                >
-                  Twitter
-                </a>
+              </a>
+              <a
+                href="https://twitter.com/TepyThai"
+                target="_blank"
+                rel="noopener"
+                className="flex justify-center items-center bg-twitter text-center text-white rounded-full max-w-xs cursor-pointer hover:bg-gray-200 hover:text-twitter transform hover:-translate-y-1 transition duration-200 ease-in-out"
+              >
+                <span className="mr-2">Twitter</span>
                 <FaTwitter />
-              </li>
-            </ul>
+              </a>
+            </div>
             <div className="mt-4">Also, you can read more about me here:</div>
             <Link href="/writing/my-journey-to-the-programming-world">
               <div className="border-2 border-primary hover:text-white hover:bg-primary bg-white px-4 py-2 mx-auto rounded-md mt-8 text-primary max-w-xs text-center cursor-pointer transform hover:-translate-y-1 transition duration-200 ease-in-out">
